31dc8b5f4c7534fb59adcd779065c9eacc599a52
Golannnnn <forburacc@protonmail.ch>
2022-07-20 17:28:34 +0300

    added createHeader function in index.js
    creates and handles the header elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+import './style.css';
 import { menuMessage } from './menu.js';
 import { aboutMessage } from './about.js';
 
 console.log('Helllooo');
 menuMessage();
 aboutMessage();
+
+const createHeader = (() => {
+    const content = document.querySelector('#content');
+    const header = document.createElement('header');
+    const home = document.createElement('div');
+    const menu = document.createElement('div');
+    const about = document.createElement('div');
+
+    home.textContent = 'Home';
+    menu.textContent = 'Menu';
+    about.textContent = 'About';
+
+    content.appendChild(header);
+    header.append(home, menu, about);
+})();
